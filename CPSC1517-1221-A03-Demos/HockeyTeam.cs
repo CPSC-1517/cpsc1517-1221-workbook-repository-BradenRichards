ba9f8bc317ee052d8ebcd968936c75a01ccda044
BradenRichards <braden.alan.richards@gmail.com>
2022-09-06 14:51:03 -0600

    Added Hockey Team public Class

---
+++ CPSC1517-1221-A03-Demos/HockeyTeam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class HockeyTeam
+{
+	public HockeyTeam()
+	{
+		public enum NHLConference { Eastern, Western };
+
+		public enum NHLDivision { Pacific, Atlantic, Central, Metropolitan };
+
+	//Define data fields for storing data
+	private NHLConference _conference;
+	private NHLDivision _division;
+
+	//Define fully-implemented properties for the data fields
+	public NHLConference Conference
+	{
+		get { return _conference; }
+	}
+
+	public NHLDivision Division
+	{
+		get { return _division; }
+	}
+
+	}
+}
