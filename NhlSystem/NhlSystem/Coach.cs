e2e9b833f7bf3dda4488605611e34f1caedd4feb
BradenRichards <braden.alan.richards@gmail.com>
2022-09-16 11:53:01 -0600

    Sept16 Created Person and Tested

---
+++ NhlSystem/NhlSystem/Coach.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystem
+{
+    //Define a class named Coach that inherits from the base class person
+    public class Coach : Person
+    {
+        //Define an auto-implemented property for HireDate
+        public DateTime HireDate { get; set; }
+
+        //Define a constructor that passes FullName to the Person base class
+
+        public Coach(string fullName, DateTime hireDate) : base(fullName)
+        {
+            HireDate = hireDate;
+        }
+    }
+}
---
+++ NhlSystem/NhlSystem/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystem
+{
+    public class Player : Person
+    {
+
+        public Position Position { get; set; }
+
+        public int Goals { get; set; }
+
+        public int Assists { get; set; }
+
+        public int PrimaryNo { get; set; }
+
+        public Player(string _fullName, Position position, int primaryNo) : base(_fullName)
+        {
+            Position = position;
+            PrimaryNo = primaryNo;
+            Goals = 0;
+            Assists = 0;
+        }
+
+        public Player(string _fullName ,Position position, int goals, int assists, int primaryNo) : base(_fullName)
+        {
+            Position = position;
+            Goals = goals;
+            Assists = assists;
+            PrimaryNo = primaryNo;
+        }
+    }
+}
