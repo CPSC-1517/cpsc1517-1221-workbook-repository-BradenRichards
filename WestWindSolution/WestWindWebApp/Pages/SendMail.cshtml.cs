a7c82b71ad3bd3c838580bb62339acd736fc07b8
Braden RICHARDS <brichards11@nait.ca>
2022-10-18 14:33:23 -0600

    Completed Email Posting

--- WestWindSolution/WestWindWebApp/Pages/SendMail.cshtml.cs
+++ WestWindSolution/WestWindWebApp/Pages/SendMail.cshtml.cs
@@ -2,11 +2,31 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Net;
 using System.Net.Mail;
+using System.Security.Cryptography;
 
 namespace WestWindWebApp.Pages
 {
     public class SendMailModel : PageModel
     {
+
+        public void OnGet()
+        {
+            //var gmailUsername = Configuration["GmailCredentials:Username"];
+            //var gmailAppPassword = Configuration["GmailCredentials:Password"];
+            //FeedbackMessage = $"Gmail Username = {gmailUsername} <br />";
+            //FeedbackMessage += $"Gmail App Password = {gmailAppPassword} <br />";
+        }
+
+        private readonly IConfiguration Configuration;
+
+        public SendMailModel(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        [BindProperty]
+        public string FeedbackMessage { get; set; }
+
         [BindProperty]
         public string MailToAddress { get; set; }
         [BindProperty]
@@ -18,21 +38,21 @@ namespace WestWindWebApp.Pages
         [BindProperty]
         public string MailMessage { get; set; }
 
-        public void OnGet()
+        public void OnPostSendMail()
         {
-            //MailUsername = "johnnysins@gmail.com";
-            //MailPassword = "manOFmanyTalents";
-            //MailToAddress = "swu@nait.ca";
-            //MailSubject = "CPSC1517 Send Mail demo";
-            //MailMessage = "This email was sent from a Razor Page";
+            FeedbackMessage = "<h2>Send Mail button clicked</h2>";
 
             var sendMailClient = new SmtpClient();
-            sendMailClient.Host = "localhost";
+            sendMailClient.Host = "smtp.gmail.com";
             sendMailClient.Port = 587;
             sendMailClient.EnableSsl = true;
+
             var sendMailCredentials = new NetworkCredential();
+            MailUsername = Configuration["GmailCredentials:Username"];
+            MailPassword = Configuration["GmailCredentials:Password"];
             sendMailCredentials.UserName = MailUsername;
             sendMailCredentials.Password = MailPassword;
+
             sendMailClient.Credentials = sendMailCredentials;
 
             var sendFromAddress = new MailAddress(MailUsername);
@@ -41,8 +61,28 @@ namespace WestWindWebApp.Pages
             var mailMessage = new MailMessage(sendFromAddress, sendToAddress);
             mailMessage.Subject = MailSubject;
             mailMessage.Body = MailMessage;
-            sendMailClient.Send(mailMessage);
+
+            try
+            {
+                sendMailClient.Send(mailMessage);
+                //Clear the form fields associated with the properties below
+                MailSubject = "";
+                MailMessage = "";
+                MailToAddress = "";
+                FeedbackMessage = "<div class='alert alert-primary'>Email Sent!</div>";
+            }
+            catch(Exception)
+            {
+                FeedbackMessage = "<div class='alert alert-danger'>Error sending email</div>";
+            }
+            
         }
+
+        public void OnPostClearForm()
+        {
+            FeedbackMessage = "<h2>Clear Form button clicked</h2>";
+        }
+
 
     }
 }
